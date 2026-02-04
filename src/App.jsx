3cc5a9bcee72d05503c3ca260e5d96753a2c67ab
Akash Tirkey <akash.tirkeywork@gmail.com>
2026-02-04 19:18:37 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import './App.css'
+import DisplayName from './DisplayName'
+
+function App() {
+  
+
+  return (
+    <>
+      <DisplayName/>
+    </>
+  )
+}
+
+export default App
---
+++ src/DisplayName.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const DisplayName = () => {
+  //states
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const[fullName,setFullName]=useState("");
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+
+setFullName(`${firstName} ${lastName}`);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Full Name Display</h1>
+      <label style={{display:"block"}}>
+        FirstName:
+        <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} required/>
+      </label>
+      <label style={{display:"block"}} >Last Name:
+        <input type="text"  value={lastName} onChange={(e)=> setLastName(e.target.value)} required />
+      </label>
+      <button type="submit">Submit</button>
+
+      {fullName && <h2>Full Name: {fullName}</h2>}
+    </form>
+  );
+};
+
+export default DisplayName;
